615add68030befd4840fef3e40ae2898f8256604
Rivaldi <rivaldi.saputra@jurnal.id>
2020-04-06 00:23:27 +0700

    Modify to count number of words

--- ch01/WC.hs
+++ ch01/WC.hs
@@ -2,5 +2,5 @@
 -- lines beginning with "--" are comments.
 
 main = interact wordCount
-    where wordCount input = show (length (lines input)) ++ "\n"
+    where wordCount input = show (length (words input)) ++ "\n"
 
