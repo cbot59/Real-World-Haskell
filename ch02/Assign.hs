e3b58b8853408e10a31858215f6215dae89f0a4b
Rivaldi <rivaldi.saputra@jurnal.id>
2020-04-12 16:07:51 +0700

    Add some examples

---
+++ ch02/Assign.hs
@@ -0,0 +1,5 @@
+-- file: ch02/Assign.hs
+x = 10
+x = 11
+
+-- x will be error 'Multiple declarations', functional bruh!
---
+++ ch03/add.hs
@@ -0,0 +1,4 @@
+-- file: ch03/add.hs
+add a b = a + b
+
+-- won't need any 'return' keyword to return a value
